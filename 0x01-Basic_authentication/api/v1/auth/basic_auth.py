f62814df00c00df91de60f06224ecbe97b71953f
Mohammed <mrcloud.med@gmail.com>
2024-09-04 03:02:31 +0200

    baisc

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -79,9 +79,6 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Retrieves the user from a request.
-        """
-    def current_user(self, request=None) -> TypeVar('User'):
         """ overloads Auth and retrieves the User instance for a request """
         auth_header = self.authorization_header(request)
 
