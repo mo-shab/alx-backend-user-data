2a95e3eb56d818b2520fe1ba22b44f75d8850075
Mohammed <mrcloud.med@gmail.com>
2024-09-04 02:47:06 +0200

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,9 +10,20 @@ class Auth():
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False - path and excluded_paths
         """
-        return False
+            - Returns True if path is None
+            - Returns True if excluded_paths is None or empty
+            - Returns False if path is in excluded_paths
+        """
+        if path is 'None':
+            return True
+        if not excluded_paths:
+            return True
+        normalized_path = path.rstrip('/')
+        for execlude in excluded_paths:
+            if normalized_path == execlude.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """that returns None
