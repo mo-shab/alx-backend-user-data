4f1ae055812481f4564acf4c359ab23e02ab0971
Mohammed <mrcloud.med@gmail.com>
2024-09-04 02:37:11 +0200

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,15 +8,16 @@ from typing import List, TypeVar
 class Auth():
     """ Class Auth
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """that returns None
         """
-        return False
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """that returns None
