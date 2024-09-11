dfc2b3cf5e4b903a7505a8a4c54657b964b04572
Mohammed <mrcloud.med@gmail.com>
2024-09-11 23:05:21 +0000

    users not user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
